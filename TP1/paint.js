b8631294ddf4ae9d54d7c36ab933ee0663dbac29
Juan Cruz Deccechis <juandeccechis@gmail.com>
2020-09-13 20:57:32 -0300

    lapiz, goma, cambio de grosor y color

--- TP1/paint.js
+++ TP1/paint.js
@@ -1,32 +1,95 @@
-document.querySelector("#rectangulo").addEventListener("click", crearRectangulo);
+"use strict";
+
 let ctx = document.querySelector("#canvas").getContext("2d");
 let figura = "";
 let widthInicial = 0;
 let heightInicial = 0;
+let grosor = "1";
+let color = "black";
+let sigueDibujando = false;
 let posicionesIniciales = document.querySelector("#canvas").getBoundingClientRect();
 
-function crearRectangulo() {
-    console.log("empieza");
-    document.querySelector("#canvas").addEventListener("mousedown", empiezaDibujar);
+
+document.querySelector("#rectangulo").addEventListener("click", dibujarRectangulo);
+document.querySelector("#lapiz").addEventListener("click", dibujar);
+document.querySelector("#goma").addEventListener("click", borrar);
+document.querySelector("#grosor").addEventListener("change", cambiarGrosor);
+document.querySelector("#colorLapiz").addEventListener("change", cambiarColor);
+
+
+function dibujarRectangulo() {
+    document.querySelector("#canvas").addEventListener("mousedown", empezarDibujar);
     figura = "rectangulo";
+    sigueDibujando = false;
 }
 
-function empiezaDibujar(event) {
+function cambiarGrosor() {
+    grosor = document.querySelector("#grosor").value;
+}
+
+function cambiarColor() {
+    color = document.querySelector("#colorLapiz").value;
+}
+
+function borrar() {
+    color = "white";
+    document.querySelector("#canvas").addEventListener("mousedown", empezarDibujar);
+    figura = "linea";
+    sigueDibujando = true;
+}
+
+function dibujar() {
+    color = document.querySelector("#colorLapiz").value;
+    document.querySelector("#canvas").addEventListener("mousedown", empezarDibujar);
+    figura = "linea";
+    sigueDibujando = true;
+}
+
+function empezarDibujar(event) {
     widthInicial = event.clientX - posicionesIniciales.left;
     heightInicial = event.clientY - posicionesIniciales.top;
     
-    document.querySelector("#canvas").removeEventListener("mousedown", empiezaDibujar);
+    document.querySelector("#canvas").removeEventListener("mousedown", empezarDibujar);
     document.querySelector("#canvas").addEventListener("mouseup", terminaDibujar);
+    if (figura === "linea"){
+        document.querySelector("#canvas").addEventListener("mousemove", seguirDibujando);
+    }
+}
+
+function seguirDibujando(event) {
+    let widthFinal = event.clientX  - posicionesIniciales.left;
+    let heightFinal = event.clientY - posicionesIniciales.top;
+    ctx.beginPath(); 
+    ctx.lineWidth = grosor; 
+    ctx.strokeStyle = color;
+    ctx.moveTo(widthInicial, heightInicial);
+    ctx.lineTo(widthFinal, heightFinal);
+    widthInicial = widthFinal;
+    heightInicial = heightFinal;
+    ctx.stroke();
 }
 
 function terminaDibujar(event) {
     let widthFinal = event.clientX  - posicionesIniciales.left;
     let heightFinal = event.clientY - posicionesIniciales.top;
     document.querySelector("#canvas").removeEventListener("mouseup", terminaDibujar);
+    if (sigueDibujando) {
+        document.querySelector("#canvas").removeEventListener("mousemove", seguirDibujando);
+        sigueDibujando = false;
+    }
+    ctx.beginPath(); 
+    ctx.lineWidth = grosor; 
+    ctx.strokeStyle = color;
     switch (figura) {
         case "rectangulo":
+            ctx.fillStyle = color;
             ctx.fillRect(widthInicial, heightInicial, widthFinal - widthInicial, heightFinal - heightInicial);
-            break;    
+            break;
+        case "linea":
+            ctx.moveTo(widthInicial, heightInicial);
+            ctx.lineTo(widthFinal, heightFinal);
+            ctx.stroke();
+            break;
         default:
             break;
     }
