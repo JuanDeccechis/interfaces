b904d88b8c047b037df46a5c9d37bc05d4908b5e
Juan Cruz Deccechis <juandeccechis@gmail.com>
2020-09-15 21:37:26 -0300

    cargar y descargar imagen

--- TP1/manejoArchivos.js
+++ TP1/manejoArchivos.js
@@ -1,6 +1,8 @@
 "use strict";
 
 document.querySelector("#cargarImagen").addEventListener("change", cargarArchivo);
+document.querySelector("#descargarEditada").addEventListener("click", descargarEditada);
+document.querySelector("#descargarFiltrada").addEventListener("click", descargarFiltrada);
 
 function cargarArchivo() {
     let archivo = document.querySelector("#cargarImagen").files[0];
@@ -11,7 +13,39 @@ function cargarArchivo() {
         let image = new Image();
         image.src = content;
         image.onload = function() {
-            ctx.drawImage(this, 0, 0);
+            while (image.width > canvas.width){
+                image.width = 0.8 * image.width;
+                image.height = 0.8 * image.height;
+            }
+            while (image.height > canvas.height){
+                image.width = 0.8 * image.width;
+                image.height = 0.8 * image.height;
+            }
+            ctx.fillStyle = "white";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(image, 0, 0, image.width, image.height);
         }
     }
+}
+
+function descargarEditada() {
+    if (document.getElementById("linkDescarga")){
+        document.getElementById("linkDescarga").remove();
+    }
+    let link = document.createElement("a");
+    link.id = "linkDescarga";
+    link.download = "imageDownloadedFromCanvas.png";
+    link.href = document.querySelector("#canvas").toDataURL("image/png").replace("image/png", "image/octet-stream");
+    link.click();   
+}
+
+function descargarFiltrada() {
+    if (document.getElementById("linkDescarga")){
+        document.getElementById("linkDescarga").remove();
+    }
+    let link = document.createElement("a");
+    link.id = "linkDescarga";
+    link.download = "imageDownloadedFromCanvas.png";
+    link.href = document.querySelector("#canvasResultadoFiltro").toDataURL("image/png").replace("image/png", "image/octet-stream");
+    link.click();   
 }
