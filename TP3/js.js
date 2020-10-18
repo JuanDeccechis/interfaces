17208bdf9513da6fb012531face7c104e57717fc
Juan Cruz Deccechis <juandeccechis@gmail.com>
2020-10-18 11:56:11 -0300

    links con cargando, primer version del carrusel

--- TP3/js.js
+++ TP3/js.js
@@ -14,19 +14,37 @@ setInterval(() => {
 }, 1000);
 
 document.addEventListener("scroll", getScroll);
-document.addEventListener("DOMContentLoaded", cargar);
+document.addEventListener("DOMContentLoaded", cargarInicio);
+document.querySelector(".cambiaSeccion1").addEventListener("click", () => recargar(0));
+document.querySelector(".cambiaSeccion2").addEventListener("click", () => recargar(1.5));
+document.querySelector(".cambiaSeccion3").addEventListener("click", () => recargar(3));
+
+function cargarInicio() {
+    cargar();
+    document.querySelector(".navbar-toggler").addEventListener("click", setheightNav);
+}
 
 function cargar() {
-    document.querySelector(".navbar-toggler").addEventListener("click", setheightNav);
     setTimeout(() => {
         let componentesActivables = document.querySelectorAll(".oculto");
         for (let index = 0; index < componentesActivables.length; index++) {
             componentesActivables[index].classList.remove("oculto");
         }
-        setheightNav();
-        document.querySelector(".cargando").classList.add("oculto");
+        document.querySelector(".imagenCargando").classList.add("oculto");
+        document.querySelector(".imagenCargando").classList.remove("cargando");
     }, 3000);
     
+}
+
+function recargar(param) {
+    document.querySelector(".imagenCargando").classList.remove("oculto");
+    document.querySelector(".imagenCargando").classList.add("cargando");
+    cargar();
+    setTimeout(() => {
+        window.innerHeight * param + heightNav;
+        window.scrollTo(0,  window.innerHeight * param + heightNav);
+        console.log(window.innerHeight * param + heightNav);
+    }, 3000);
 }
 
 function setheightNav() {
@@ -52,6 +70,7 @@ function getScroll(evt) {
         case 1:
             console.log("baja seccion 1");
             document.querySelector(".pag1").classList.remove("animaEntrada1");
+            document.querySelector(".pag2").classList.remove("animaEntrada2");
             seccionActual = 1;
             break;
         case 2:
@@ -69,14 +88,17 @@ function getScroll(evt) {
         case 11:
             console.log("sube seccion 1");
             document.querySelector(".pag1").classList.remove("animaEntrada1");
+            document.querySelector(".pag2").classList.remove("animaEntrada2");
             break;
         case 12:
             console.log("sube seccion 2");
-            document.querySelector(".pag1").classList.remove("animaEntrada2");
+            document.querySelector(".pag2").classList.remove("animaEntrada2");
             document.querySelector(".pag1").classList.add("animaEntrada1");
             break;
         case 13:
             console.log("sube seccion 3");
+            document.querySelector(".pag2").classList.remove("animaEntrada2");
+            document.querySelector(".pag1").classList.remove("animaEntrada1");
             break;
         default:
             break;
@@ -156,3 +178,19 @@ function getScrollY() {
     return scrOfY;
 }
 
+let cards = document.querySelectorAll(".card");
+for (let index = 0; index < cards.length; index++) {
+    cards[index].addEventListener("mousemove", (evento) => rotarCard(cards[index], evento));
+    cards[index].addEventListener("mouseleave", function() {
+        cards[index].style.transform = `rotateY(${0}deg) rotateX(${0}deg)`;
+    })
+}
+
+function rotarCard(elemento, evento) {
+    let posiciones = elemento.getBoundingClientRect();
+    //roto 1/5 y 1/3 de la diferencia entre el centro del card y donde clickea adentro del card
+    let xAxis = (posiciones.width / 2 - (evento.clientX - posiciones.x)) / 5;
+    let yAxis = (posiciones.height / 2 - (evento.clientY - posiciones.y)) / 10;
+    
+    elemento.style.transform = `rotateY(${-xAxis}deg) rotateX(${yAxis}deg)`;
+};
